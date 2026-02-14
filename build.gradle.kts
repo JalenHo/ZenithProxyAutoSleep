800687b06c9d6de1ac27a8537a13462bfc315e0e
Jalen Ho <hokayiuonminecraftpe@gmail.com>
2026-02-14 23:58:54 +0800

    Switch to mc=1.21.4 + Java 21 for 2b2t compatibility, update .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ version = properties["plugin_version"] as String
 val mc = properties["mc"] as String
 val pluginId = properties["plugin_id"] as String
 
-java { toolchain { languageVersion = JavaLanguageVersion.of(25) } }
+java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
 zenithProxyPlugin {
     templateProperties = mapOf(
@@ -18,7 +18,7 @@ zenithProxyPlugin {
         "maven_group" to group as String,
     )
     // the minimum supported java version for users of your plugin
-    javaReleaseVersion = JavaLanguageVersion.of(25)
+    javaReleaseVersion = JavaLanguageVersion.of(21)
 }
 
 repositories {
